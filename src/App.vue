881ec86b44bf50f138107ed6a7042ccbd86e7dac
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-04-13 15:37:49 +0200

    add index vue and install everything

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="bg-light">header</header>
+  <main>
+    <section class="bg-dark"> prima section</section>
+    <section class="bg-primary">seconda section</section>
+  </main>
+  <footer>
+    <section> prima section footer con immagine</section>
+    <section class="bg-dark"> footer</section>
+  </footer>
+
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
