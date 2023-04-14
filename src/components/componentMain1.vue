d767b5bc0a4ac974ce9ef9a2a4bfeec2734056a4
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-04-14 17:08:33 +0200

    first try props

--- src/components/componentMain1.vue
+++ src/components/componentMain1.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class="container p-5">
-<h1 class="text-light"> --INSERIRE CONTENUTO QUI!--</h1>
+    <div class="d-flex flex-column align-content-center justify-content-center py-2">
+        <div class="gi-img mb-2">
+            <img :src="thumb" :alt="type">
+        </div>
+        <h6 class="text-light text-center">{{ series }}</h6>
+        
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    props: {
+        thumb: 'string',
+        series: 'string',
+        type: 'string'
     }
+}
 </script>
 
 <style lang="scss" scoped>
+.gi-img {
+    width: 200px;
+    height: 200px;
 
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+
+    }
+}
 </style>
